b39f153ab3f7a1d3665eb42c361d8cd0ac8329d9
João Pedro Oliveira <joao.santos@comadre.com.br>
2022-03-21 12:41:19 -0300

    add guardian and hound as deps

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Fsc.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:guardian, "~> 2.0"}
+      {:guardian, "~> 2.0"},
+      {:hound, "~> 1.0"}
     ]
   end
 
